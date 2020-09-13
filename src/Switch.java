271b74038e5b9f854e9a65a67f7950a734687534
aprilhoward <apehow111@gmail.com>
2020-09-13 18:15:58 -0400

    Initial commit

---
+++ src/Switch.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Switch {
+
+
+    public static void Switch() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter the  1, 2, 3, 4, 5, 6, or 7");
+        int switchValue = scan.nextInt();
+
+        switch (switchValue) {
+            case 1:
+                System.out.println("Value entered was 1");
+                break;
+            case 2:
+                System.out.println("Value entered was 2");
+                break;
+            case 3:
+                System.out.println("Value entered was 3");
+                break;
+            case 4:
+                System.out.println("Value entered was 4");
+                break;
+            case 5: case 6: case 7:
+                System.out.println("Value entered was a 5, or a 6, or a 7");
+                System.out.println("More precisely, it was a " + switchValue);
+                break;
+            default:
+                System.out.println("Invalid entry");
+                Switch();
+        }
+    }
+
+}
---
+++ src/SwitchChar.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class SwitchChar {
+
+    public static void SwitchChar() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\nPlease enter a letter of the alphabet.");
+        String switchChar = scanner.next();
+
+        switch(switchChar) {
+
+            case "A": case "a": case "B": case "b": case "C": case "c":
+            case "D": case "d": case "E": case "e": case "F": case "f":
+            case "G": case "g": case "H": case "h": case "I": case "i":
+            case "J": case "j": case "K": case "k": case "L": case "l":
+            case "M": case "m": case "N": case "n": case "O": case "o":
+            case "P": case "p": case "Q": case "q": case "R": case "r":
+            case "S": case "s": case "T": case "t": case "U": case "u":
+            case "V": case "v": case "W": case "w": case "X": case "x":
+            case "Y": case "y": case "Z": case "z":
+                System.out.println("you entered " + switchChar);
+                break;
+            default:
+                System.out.println("Oops, that's an invalid input!");
+
+        }
+        SwitchChar();
+    }
+}
